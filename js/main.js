3621f5090be50bce8601b342360a97a7691d9146
Koło <poprostukolo@gmail.com>
2024-01-18 23:39:58 +0100

    Added page named "skills" and some change in RWD

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const body = document.querySelector('body')
 const lightBtn = document.querySelector('.btns__light-mode')
 const darkBtn = document.querySelector('.btns__dark-mode')
 
+const menuSections = document.querySelectorAll('.menu__sections')
+const menuBtns = document.querySelectorAll('.menu__btn')
+
 const lightMode = () => {
 	body.setAttribute('data-theme', 'light')
 	lightBtn.classList.add('btns__btn--active')
@@ -12,6 +15,17 @@ const darkMode = () => {
 	body.setAttribute('data-theme', 'dark')
 	lightBtn.classList.remove('btns__btn--active')
 	darkBtn.classList.add('btns__btn--active')
+}
+
+const showSection = id => {
+	menuSections.forEach(section => section.classList.add('d-none'))
+	menuBtns.forEach(btn => btn.classList.remove('btn-success'))
+	menuBtns.forEach(btn => btn.classList.add('btn-outline-secondary'))
+	document.getElementById(id).classList.remove('d-none')
+
+	const currentActiveBtn = document.querySelector(`[data-id=${id}]`)
+	currentActiveBtn.classList.remove('btn-outline-secondary')
+	currentActiveBtn.classList.add('btn-success')
 }
 
 lightBtn.addEventListener('click', lightMode)
